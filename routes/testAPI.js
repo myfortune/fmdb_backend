edf041f9afafd1e4331eab1080dc84d12d955817
myfortune <sang.ahn@mail.utoronto.ca>
2022-12-22 19:31:34 -0500

    adding a new test path

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,11 +1,27 @@
 let express = require('express');
 let router = express.Router();
-const {PSQL_REPOSITORY} = require("../db/psql");
+//const {PSQL_REPOSITORY} = require("../db/psql");
 
 router.get('/',  async function(req, res, next) {
 
     try {
         res.json(await new PSQL_REPOSITORY().getAllPlayers());
+
+
+    } catch(err){
+        console.error(`Error while getting quotes `, err.message);
+        next(err);
+    }
+});
+
+router.get('/:cardId',  async function(req, res, next) {
+
+    try {
+        //res.json(await new PSQL_REPOSITORY().getAllPlayers());
+        let cardId = parseInt(req.params.cardId) + 1;
+        console.log(cardId);
+        res.json(cardId);
+
     } catch(err){
         console.error(`Error while getting quotes `, err.message);
         next(err);
