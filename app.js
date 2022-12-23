c11916fdfd888c0c6240e3cb64f7037e3c8d236d
myfortune <sang.ahn@mail.utoronto.ca>
2022-12-22 22:53:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ let port = process.env.PORT || CONFIG.port || DEFAULT_PORT;
 server.listen(port, function(){
     console.log(process.env.DATA_URL)
     console.log(express().get("env"))
-    console.log("123");
+    console.log("1000");
     console.log(`Listening on port ${port}`)
 });
 
